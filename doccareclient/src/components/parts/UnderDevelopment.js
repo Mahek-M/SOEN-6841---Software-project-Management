98c80af609036579555d145088b37edd17d10076
Priyanshi <Priyanshi431999@gmail.com>
2022-10-29 00:01:58 +0600

    added under development page and it's scss

---
+++ doccareclient/src/components/parts/UnderDevelopment.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./UnderDevelopment.scss";
+
+const UnderDevelopment = ()=> {
+    return (
+        <div className="underdev-container">
+             <h1>UnderDevelopment</h1>
+             <h5>This page is under development.<br/>Wait for the next sprint to use this feature.</h5>
+        </div>
+    );
+}
+
+export default UnderDevelopment;
