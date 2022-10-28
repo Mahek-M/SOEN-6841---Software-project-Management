8d1ce5af4aad22766f340d7dee3e54c3b81dfdad
Yashvi <yashvi.pithadia.it@gmail.com>
2022-10-29 00:21:46 +0600

    added backend for the project

---
+++ doccare/src/main/java/com/doccare/doccare/DoccareApplication.java
@@ -0,0 +1,13 @@
+package com.doccare.doccare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoccareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoccareApplication.class, args);
+	}
+
+}
